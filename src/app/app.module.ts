4a82a39d3b6d96f8b4719d80e90d464a3aed4942
portm4n <ruben_sanfer_3@hotmail.com>
2020-05-23 11:23:57 +0200

    continua

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
@@ -9,7 +11,8 @@ import { SearchComponent } from './components/search/search.component';
 import { ArtistaComponent } from './components/artista/artista.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
 
-//Importar rutas
+// Importar rutas
+
 import { ROUTES } from './app.routes';
 
 @NgModule({
@@ -22,7 +25,7 @@ import { ROUTES } from './app.routes';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    HttpClientModule,
     RouterModule.forRoot(ROUTES, { useHash: true }),
   ],
   providers: [],
